146052ffe0975b6379febeee63f620b8e8d5e346
Tero Parvinen <tero.parvinen@gmail.com>
2022-01-20 17:26:56 +0200

    Reformatting, a bug fix and some more meta data

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sheet Commander for Foundry VTT
+
+Allows quick movement of sheet windows by pressing down only a configurable modifier key (optionally combination) anywhere on the sheet. Also allows binding a keyboard shortcut to close the sheet under the cursor or the active sheet.
+
+## Usage
+
+The modifier key combination set can be changed in module settings. Options include Meta (Cmd on Macs, Win on Windows), Alt, Control and Shift. Selecting multiple requires all the modifiers given to be pressed for the functionality to work. Deselecting all the options disables the functionality entirely.
+
+A key binding can be set in control configuration to close sheets using a hotkey. There are two options.
+
+- Close window under mouse: Targets the sheet under the cursor
+- Close active window: While Foundry has the concept of an active window, the UI gives very little feedback about this. This option is here if you need it, but I recommend using the cursor based option.
+
+## License
+
+This Foundry VTT module, written by Tero Parvinen, is licensed under a Creative Commons Attribution 4.0 International License.
+
+This work is licensed under the Foundry Virtual Tabletop EULA - Limited License Agreement for module development.
--- module/sheet-commander.js
+++ module/sheet-commander.js
@@ -8,125 +8,146 @@ let altKey = false;
 let ctrlKey = false;
 let shiftKey = false;
 
+let state = {
+    trackingId: null
+}
+
 function startMonitoring(app, html) {
-	html.on('mousemove', function (event) {
-		let isAccepted = false;
+    html.on('mousemove', function (event) {
+        let isAccepted = false;
 
-		if ((metaKey && event.metaKey || (!metaKey && !event.metaKey)) &&
-			(altKey && event.altKey || (!altKey && !event.altKey)) &&
-			(ctrlKey && event.ctrlKey || (!ctrlKey && !event.ctrlKey)) &&
-			(shiftKey && event.shiftKey || (!shiftKey && !event.shiftKey))) {
-				isAccepted = true;
-			}
+        if ((metaKey && event.metaKey || (!metaKey && !event.metaKey)) &&
+            (altKey && event.altKey || (!altKey && !event.altKey)) &&
+            (ctrlKey && event.ctrlKey || (!ctrlKey && !event.ctrlKey)) &&
+            (shiftKey && event.shiftKey || (!shiftKey && !event.shiftKey))) {
+                isAccepted = true;
+            }
 
-		if (isAccepted && !this.isActive) {
-			// Move start
-			this.isActive = true;
-			this.appStart = {
-				left: app.position.left,
-				top: app.position.top
-			}
-			this.mouseStart = {
-				left: event.pageX,
-				top: event.pageY
-			}
-		} else if (!isAccepted && this.isActive) {
-			// Move end
-			this.isActive = false;
-		}
+        if (isAccepted && !state.trackingId) {
+            // Move start
+            state.trackingId = app.id;
+            state.appStart = {
+                left: app.position.left,
+                top: app.position.top
+            }
+            state.mouseStart = {
+                left: event.pageX,
+                top: event.pageY
+            }
+        } else if (!isAccepted && state.trackingId) {
+            // Move end
+            state.trackingId = null;
+        }
 
-		if (this.isActive) {
-			const left = this.appStart.left + event.pageX - this.mouseStart.left;
-			const top = this.appStart.top + event.pageY - this.mouseStart.top;
-			app.setPosition({ left, top });
-		}
-	}.bind({
-		isActive: false
-	}));
+        if (state.trackingId == app.id) {
+            const left = state.appStart.left + event.pageX - state.mouseStart.left;
+            const top = state.appStart.top + event.pageY - state.mouseStart.top;
+            app.setPosition({ left, top });
+        }
+    });
 
-	html.on('mouseenter', function(event) {
-		lastSeenApp = app;
-	});
-	html.on('mouseleave', function(event) {
-		if (lastSeenApp === app) {
-			lastSeenApp = null;
-		}
-	});
+    html.on('mouseenter', function(event) {
+        lastSeenApp = app;
+    });
+    html.on('mouseleave', function(event) {
+        if (lastSeenApp === app) {
+            lastSeenApp = null;
+        }
+    });
+}
+
+function stopMonitoring(app) {
+    if (state.trackingId == app.id) {
+        state.trackingId = null;
+    }
 }
 
 Hooks.on('renderFormApplication', (app, html) => {
-	startMonitoring(app, html);
+    startMonitoring(app, html);
 });
 Hooks.on('renderActorSheet', (app, html) => {
-	startMonitoring(app, html);
+    startMonitoring(app, html);
 });
 Hooks.on('renderItemSheet', (app, html) => {
-	startMonitoring(app, html);
+    startMonitoring(app, html);
 });
 Hooks.on('renderJournalSheet', (app, html) => {
-	startMonitoring(app, html);
+    startMonitoring(app, html);
 });
 Hooks.on('renderCompendium', (app, html) => {
-	startMonitoring(app, html);
+    startMonitoring(app, html);
 });
 Hooks.on('renderSidebarTab', (app, html) => {
-	if (app !== app._original) {
-		startMonitoring(app, html);
-	}
+    if (app !== app._original) {
+        startMonitoring(app, html);
+    }
+});
+
+Hooks.on('closeApplication', (app) => {
+    stopMonitoring(app);
+});
+Hooks.on('closeActorSheet', (app) => {
+    stopMonitoring(app);
+});
+Hooks.on('closeItemSheet', (app) => {
+    stopMonitoring(app);
+});
+Hooks.on('closeSidebarTab', (app) => {
+    stopMonitoring(app);
 });
 
 Hooks.once('setup', () => {
-	game.keybindings.register(moduleId, 'keybinding-under-mouse', {
-		name: `${localizationId}.close-under-mouse-hotkey`,
-		editable: [],
-		onDown: (ctx) => {
-			lastSeenApp?.close();
-		}
-	});
-	game.keybindings.register(moduleId, 'keybinding-active', {
-		name: `${localizationId}.close-active-hotkey`,
-		editable: [],
-		onDown: (ctx) => {
-			ui.activeWindow?.close();
-		}
-	});
+    game.keybindings.register(moduleId, 'keybinding-under-mouse', {
+        name: `${localizationId}.close-under-mouse-hotkey`,
+        editable: [],
+        onDown: (ctx) => {
+            lastSeenApp?.close();
+        }
+    });
+    game.keybindings.register(moduleId, 'keybinding-active', {
+        name: `${localizationId}.close-active-hotkey`,
+        editable: [],
+        onDown: (ctx) => {
+            ui.activeWindow?.close();
+        }
+    });
 
-	game.settings.register(moduleId, 'meta-move', {
-		name: `${localizationId}.meta-move`,
-		hint: `${localizationId}.move-hint`,
-		scope: 'client',
-		config: true,
-		type: Boolean,
-		default: true,
-		onChange: value => { metaKey = value; }
-	});
-	game.settings.register(moduleId, 'alt-move', {
-		name: `${localizationId}.alt-move`,
-		scope: 'client',
-		config: true,
-		type: Boolean,
-		default: false,
-		onChange: value => { altKey = value; }
-	});
-	game.settings.register(moduleId, 'ctrl-move', {
-		name: `${localizationId}.ctrl-move`,
-		scope: 'client',
-		config: true,
-		type: Boolean,
-		default: false,
-		onChange: value => { ctrlKey = value; }
-	});
-	game.settings.register(moduleId, 'shift-move', {
-		name: `${localizationId}.shift-move`,
-		scope: 'client',
-		config: true,
-		type: Boolean,
-		default: false,
-		onChange: value => { shiftKey = value; }
-	});
+    game.settings.register(moduleId, 'meta-move', {
+        name: `${localizationId}.meta-move`,
+        hint: `${localizationId}.move-hint`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { metaKey = value; }
+    });
+    game.settings.register(moduleId, 'alt-move', {
+        name: `${localizationId}.alt-move`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: true,
+        onChange: value => { altKey = value; }
+    });
+    game.settings.register(moduleId, 'ctrl-move', {
+        name: `${localizationId}.ctrl-move`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { ctrlKey = value; }
+    });
+    game.settings.register(moduleId, 'shift-move', {
+        name: `${localizationId}.shift-move`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { shiftKey = value; }
+    });
 
-	metaKey = game.settings.get(moduleId, 'meta-move');
-	altKey = game.settings.get(moduleId, 'alt-move');
-	ctrlKey = game.settings.get(moduleId, 'ctrl-move');
-	shiftKey = game.settings.get(moduleId, 'shift-move');
+    metaKey = game.settings.get(moduleId, 'meta-move');
+    altKey = game.settings.get(moduleId, 'alt-move');
+    ctrlKey = game.settings.get(moduleId, 'ctrl-move');
+    shiftKey = game.settings.get(moduleId, 'shift-move');
 });
